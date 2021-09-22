9f57d108f6c554e26d0a65c2a33a89a0215c46bc
Debanjan <debanjanacharya21@gmail.com>
2021-09-23 02:09:36 +0530

    end of the project

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -24,11 +24,18 @@ export const cryptoApi = createApi({
     endpoints: (builder) => ({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timePeriod }) => createRequest(`/coin/${coinId}/history/${timePeriod}`),
+        }),
+        getExchanges: builder.query({
+            query: () => createRequest(`/exchanges`),
         })
     })
 })
 
 
-export const {
-    useGetCryptosQuery,
-} = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetExchangesQuery } = cryptoApi;
